efecfadca79106fb576981e36ac653fccb839cb7
Don Ngo <dontatngo@gmail.com>
2026-07-15 01:54:30 -0400

    Remove temporary patch scripts

--- fix_memo.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('./components/D3Graph.tsx', 'utf8');
-
-code = code.replace(
-  "const communityLabels = (Array.from(new Set(Object.values(communityMap))) as string[]).sort((a, b) => \n    a.toString().localeCompare(b.toString(), undefined, { numeric: true, sensitivity: 'base' })\n  );",
-  "const communityLabels = useMemo(() => (Array.from(new Set(Object.values(communityMap))) as string[]).sort((a, b) => \n    a.toString().localeCompare(b.toString(), undefined, { numeric: true, sensitivity: 'base' })\n  ), [communityMap]);"
-);
-
-fs.writeFileSync('./components/D3Graph.tsx', code);
